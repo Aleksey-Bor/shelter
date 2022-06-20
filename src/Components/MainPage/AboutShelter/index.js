78fff9f99afd7b53c0bab5be87e1df38248e1f37
Aliaksey <it.borki@gmail.com>
2022-06-20 12:23:26 +0300

    Change module name and class name from about pets to about sheltor

---
+++ src/Components/MainPage/AboutShelter/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from "../../../utils/htmlToElement";
+import AboutShelter from "./index.html"
+import "./index.scss"
+
+const aboutShelterElem = htmlToElement(AboutShelter);
+
+export default aboutShelterElem;
